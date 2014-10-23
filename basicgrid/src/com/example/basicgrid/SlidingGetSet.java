34de6617f3477aaeba58dc96aeb00fdb8ac6c326
Manmeet Singh Oberoi <ms_oberoi@outlook.com>
2014-10-23 13:24:26 +0530

    Added menu drawer. Will remove the new game exit buttons later
    https://github.com/gokhanakkurt/android-menudrawer
    
    yeh library use ki hai

---
+++ basicgrid/src/com/example/basicgrid/SlidingGetSet.java
@@ -0,0 +1,14 @@
+package com.example.basicgrid;
+
+public class SlidingGetSet {
+    String name;
+    public SlidingGetSet(String name){
+    	 this.name= name;
+    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
